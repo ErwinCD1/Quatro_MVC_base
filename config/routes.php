69ae91709b6b17637179abdd6a4284986c54536d
stagiaire <stagiaire@iMac21.local>
2017-04-06 16:46:39 +0200

    Groupe Quatro - Laure/Alexandre/Nicolas/Erwin

---
+++ config/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+    return array( 'routes' => array(
+        '^(.*)$' => array(
+            'controller' => 'Controller_Default',
+            'action' => 'index',
+        ),
+    ),
+);
